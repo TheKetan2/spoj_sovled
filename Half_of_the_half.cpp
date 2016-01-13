468f2ab05a689735644d65e7542dbccef8c2d6e1
Ketan Rmateke <Ketantoo@gmail.com>
2016-01-13 20:04:06 +0530

    Update Half_of_the_half.cpp
    http://www.spoj.com/problems/STRHH/

--- Half_of_the_half.cpp
+++ Half_of_the_half.cpp
@@ -5,19 +5,19 @@
 
 int main()
 {
-    char ch[100];
+    char ch[123456]; //initially I took size of array 100 and it gave me segmentation fault so i increased the array size and it worked
     int i,len=0,k,b;
-    scanf("%d",&b);
+    scanf("%d",&b); //number of string to be inputed
     for(int a=0;a<=b;a++){
         gets(ch);
         i=0;
 
+    //finding lenght of string ,you can use string function too
     while(ch[i]!=NULL)
     {
-        //printf("%c",ch[i]);
         i++;
     }
-    len = i/2;
+    len = i/2; //for finding half lenght of string
 
     for(k=0;k<len;k+=2){
         printf("%c",ch[k]);
